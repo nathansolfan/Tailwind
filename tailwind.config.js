97213a0bc4f64b05b4c936c8c723fdb2f25fbb9d
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2023-12-25 23:15:44 +0000

    created a fontSize for testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -330,6 +330,7 @@ module.exports = {
       ],
     },
     fontSize: {
+      nega: ['0.01rem', { lineHeight: '0.01rem'}],
       xs: ['0.75rem', { lineHeight: '1rem' }],
       sm: ['0.875rem', { lineHeight: '1.25rem' }],
       base: ['1rem', { lineHeight: '1.5rem' }],
